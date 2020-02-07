d9a129f433f47994536965aebe7ae127eb6d28f3
OpeyemiOluwa <idrisbalikisopeyemi@gmail.com>
2020-02-07 23:51:55 +0100

    Modify Author controller class

--- src/main/java/com/tindrah/springwebapp/controllers/AuthorController.java
+++ src/main/java/com/tindrah/springwebapp/controllers/AuthorController.java
@@ -1,4 +1,21 @@
 package com.tindrah.springwebapp.controllers;
 
+import com.tindrah.springwebapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class AuthorController {
+    AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model){
+        model.addAttribute("author", authorRepository.findAll());
+        return "author";
+    }
 }
